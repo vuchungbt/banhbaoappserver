8f76e1ae9b6fc6146ebcb92d07dc60f621001524
namhihi237 <yentth237@gmail.com>
2020-10-03 22:19:02 +0700

    find, block, ban user

--- routes/admin/room.js
+++ routes/admin/room.js
@@ -1,13 +1,15 @@
-const Room = require("../../models/Database");
-const express = require("express");
+const Room = require('../../models/Database');
+const express = require('express');
 const router = express.Router();
-const Messages = require("../../models/Message");
+const Messages = require('../../models/Message');
 
-router.get("/:page", async(req, res) => {
+router.get('/:page', async(req, res) => {
     let perPage = 15;
     let page = parseInt(req.params.page) || 1;
 
-    const rooms = await Room.find().skip(perPage * page - perPage).limit(perPage);
+    const rooms = await Room.find()
+        .skip(perPage * page - perPage)
+        .limit(perPage);
     let count = await Room.countDocuments();
     let pages = [];
     for (let i = 0; i < Math.ceil(count / perPage); i++) {
@@ -16,56 +18,54 @@ router.get("/:page", async(req, res) => {
     let prevPage, nextPage;
     prevPage = page - 1;
     nextPage = page + 1;
-    page - 1 <= 0 ? prevPage = page : prevPage = page - 1;
-    page + 1 > pages.length ? nextPage = page : nextPage = page + 1;
+    page - 1 <= 0 ? (prevPage = page) : (prevPage = page - 1);
+    page + 1 > pages.length ? (nextPage = page) : (nextPage = page + 1);
 
     console.log(prevPage, page, nextPage);
     console.log(pages);
-    res.render("room/showroom.pug", {
+    res.render('room/showroom.pug', {
         rooms,
         currentPage: page,
         prevPage,
         nextPage,
-        pages: pages
-    })
-})
+        pages: pages,
+    });
+});
 
-router.get("/getmessages/:_id", async(req, res) => {
+router.get('/getmessages/:_id', async(req, res) => {
     const room_id = req.params._id;
     var messages = await Messages.find({
-        room_id
+        room_id,
     }, {
         content: 1,
-        sender: 1
-    });
+        sender: 1,
+    }, );
     const room = await Room.findById({
-        _id: room_id
+        _id: room_id,
     });
     let mess = [];
-    let a
+    let a;
     for (let item of messages) {
         if (JSON.stringify(room.members[0]) == JSON.stringify(item.sender)) {
             a = {
                 _id: item._id,
                 sender: item.sender,
                 content: item.content,
-                check: 1
-            }
-
+                check: 1,
+            };
         } else {
             a = {
                 _id: item._id,
                 sender: item.sender,
                 content: item.content,
-            }
+            };
         }
         mess.push(a);
     }
     console.log(mess);
-    res.render("room/room.pug", {
+    res.render('room/room.pug', {
         mess,
-        room
+        room,
     });
-
-})
+});
 module.exports = router;
--- routes/admin/user.js
+++ routes/admin/user.js
@@ -1,14 +1,75 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/authAdmin")
+const User = require("../../models/User");
+const mongoose = require("mongoose");
+Date.prototype.addHours = function(h) {
+    this.setHours(this.getHours() + h);
+    return this;
+};
+
 router.get("/", auth.logged, (req, res) => {
-    const admin = req.admin;
-    res.render("user/user.pug", {
-        admin
-    })
+    res.render("user/user.pug")
 })
 
-async function countUserActive() {
+router.post("/", auth.logged, async(req, res) => {
+    const _id = req.body.id;
+    try {
+        const user = await User.findById({
+            _id
+        });
+        if (user) {
+            res.render("user/profile", {
+                user
+            })
+        }
+        res.render("user/user.pug", {
+            mess: "ID user not found"
+        })
+    } catch (error) {
+        res.render("user/user.pug", {
+            mess: "ID user not found"
+        })
+    }
+})
 
-}
+router.get("/delete/:_id", auth.logged, async(req, res) => {
+    const _id = req.params._id;
+    try {
+        await User.findByIdAndRemove({
+            _id
+        });
+        res.render("user/user.pug");
+    } catch (error) {
+        console.log(error);
+        const user = await User.findById({
+            _id
+        });
+        res.render("user/profile", {
+            user
+        });
+    }
+})
+
+router.get("/block/:_id", auth.logged, async(req, res) => {
+    const _id = req.params._id;
+    let time = new Date();
+    time = time.addHours(1);
+    try {
+        await User.findByIdAndUpdate({
+            _id
+        }, {
+            timeBlock: time
+        })
+    } catch (error) {
+        console.log(error);
+
+    }
+    const user = await User.findById({
+        _id
+    });
+    res.render("user/profile.pug", {
+        user
+    });
+})
 module.exports = router;
