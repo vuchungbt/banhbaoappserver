66bd8b3a3712cda14034971cdd6721e4f1bb9fa4
unknown <OS@DICCHUNGHO.dicentral.vn>
2021-11-26 15:04:43 +0700

    get

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -10,8 +10,48 @@ const config = require('config');
 router.post('/register', authAdmin.validateUser, async(req, res) => {
     let {
         username,
-        password,
-        key
+        password
+    } = req.body;
+    // if (key != config.get("key")) {
+    //     return res.status(400).json({
+    //         status: 400,
+    //         msg: 'Key not found',
+    //     });
+    // }
+    // username = username.toLowerCase();
+    // const admin = await Admin.findOne({
+    //     username
+    // });
+     console.log('admin',username);
+    // if (admin) {
+    //     return res.status(400).json({
+    //         status: 400,
+    //         msg: 'Admin already exists',
+    //     });
+    // }
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async(err, hash) => {
+            if (err) {
+                res.status(401).json({
+                    status: 401,
+                    msg: 'bcrypt password failed',
+                });
+            }
+            password = hash;
+            await Admin.create({
+                username,
+                password
+            });
+            res.status(200).json({
+                username,
+            });
+        });
+    });
+});
+router.get('/register', async(req, res) => {
+    let {
+        username,
+        password
     } = req.body;
     // if (key != config.get("key")) {
     //     return res.status(400).json({
